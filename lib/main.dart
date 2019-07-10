16ac58e449647456637cf0e020e9519d0b92e0d2
Yuki Hirai <yukihirai0505@gmail.com>
2019-07-10 11:41:17 +0900

    Add firebase ml kit settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,198 @@
+import 'package:ml_app/ml_detail.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-void main() => runApp(MyApp());
+const String TEXT_SCANNER = 'TEXT_SCANNER';
+const String BARCODE_SCANNER = 'BARCODE_SCANNER';
+const String LABEL_SCANNER = 'LABEL_SCANNER';
+const String FACE_SCANNER = 'FACE_SCANNER';
+
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+      theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MLHome(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MLHome extends StatefulWidget {
+  MLHome({Key key}) : super(key: key);
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState() => _MLHomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MLHomeState extends State<MLHome> {
+  static const String CAMERA_SOURCE = 'CAMERA_SOURCE';
+  static const String GALLERY_SOURCE = 'GALLERY_SOURCE';
 
-  void _incrementCounter() {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  File _file;
+  String _selectedScanner = TEXT_SCANNER;
+
+  @override
+  Widget build(BuildContext context) {
+    final columns = List<Widget>();
+    columns.add(buildRowTitle(context, 'Select Scanner Type'));
+    columns.add(buildSelectScannerRowWidget(context));
+    columns.add(buildRowTitle(context, 'Pick Image'));
+    columns.add(buildSelectImageRowWidget(context));
+
+    return Scaffold(
+        key: _scaffoldKey,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('MLKit Demo'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: columns,
+          ),
+        ));
+  }
+
+  Widget buildRowTitle(BuildContext context, String title) {
+    return Center(
+        child: Padding(
+      padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.headline,
+      ),
+    ));
+  }
+
+  Widget buildSelectImageRowWidget(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+            child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8.0),
+          child: RaisedButton(
+              color: Colors.blue,
+              textColor: Colors.white,
+              splashColor: Colors.blueGrey,
+              onPressed: () {
+                onPickImageSelected(CAMERA_SOURCE);
+              },
+              child: const Text('Camera')),
+        )),
+        Expanded(
+            child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8.0),
+          child: RaisedButton(
+              color: Colors.blue,
+              textColor: Colors.white,
+              splashColor: Colors.blueGrey,
+              onPressed: () {
+                onPickImageSelected(GALLERY_SOURCE);
+              },
+              child: const Text('Gallery')),
+        ))
+      ],
+    );
+  }
+
+  Widget buildSelectScannerRowWidget(BuildContext context) {
+    return Wrap(
+      children: <Widget>[
+        RadioListTile<String>(
+          title: Text('Text Recognition'),
+          groupValue: _selectedScanner,
+          value: TEXT_SCANNER,
+          onChanged: onScannerSelected,
+        ),
+        RadioListTile<String>(
+          title: Text('Barcode Scanner'),
+          groupValue: _selectedScanner,
+          value: BARCODE_SCANNER,
+          onChanged: onScannerSelected,
+        ),
+        RadioListTile<String>(
+          title: Text('Label Scanner'),
+          groupValue: _selectedScanner,
+          value: LABEL_SCANNER,
+          onChanged: onScannerSelected,
+        ),
+        RadioListTile<String>(
+          title: Text('Face Scanner'),
+          groupValue: _selectedScanner,
+          value: FACE_SCANNER,
+          onChanged: onScannerSelected,
+        )
+      ],
+    );
+  }
+
+  Widget buildImageRow(BuildContext context, File file) {
+    return SizedBox(
+        height: 500.0,
+        child: Image.file(
+          file,
+          fit: BoxFit.fitWidth,
+        ));
+  }
+
+  Widget buildDeleteRow(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+        child: RaisedButton(
+            color: Colors.red,
+            textColor: Colors.white,
+            splashColor: Colors.blueGrey,
+            onPressed: () {
+              setState(() {
+                _file = null;
+              });
+              ;
+            },
+            child: const Text('Delete Image')),
+      ),
+    );
+  }
+
+  void onScannerSelected(String scanner) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedScanner = scanner;
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  void onPickImageSelected(String source) async {
+    var imageSource;
+    if (source == CAMERA_SOURCE) {
+      imageSource = ImageSource.camera;
+    } else {
+      imageSource = ImageSource.gallery;
+    }
+
+    final scaffold = _scaffoldKey.currentState;
+
+    try {
+      final file = await ImagePicker.pickImage(source: imageSource);
+      if (file == null) {
+        throw Exception('File is not available');
+      }
+
+      Navigator.push(
+        context,
+        new MaterialPageRoute(
+            builder: (context) => MLDetail(file, _selectedScanner)),
+      );
+    } catch (e) {
+      scaffold.showSnackBar(SnackBar(
+        content: Text(e.toString()),
+      ));
+    }
   }
 }
